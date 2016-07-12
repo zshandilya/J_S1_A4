c418575685cf6781361562c6638c4bfe84d99575
zshandilya <zubin_shandilya@yahoo.com>
2016-07-13 01:35:24 +0530

    Assignment 4 for Java Session 1

---
+++ Assignment4.java
@@ -0,0 +1,24 @@
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		
+		int x=5, y=10;
+		int a,b,c,d;
+		
+		a=x+(y*2);
+		b=x-y+2;
+		c=(x+y)*2;
+		d=y%x;
+		
+		System.out.println("Value of x: " +x);
+		System.out.println("Value of y: " +y);
+		System.out.println("");
+		System.out.println("x+y*2   = " +a);
+		System.out.println("x-y+2   = " +b);
+		System.out.println("(x+y)*2 = " +c);
+		System.out.println("y%x     = " +d);
+
+	}
+
+}
